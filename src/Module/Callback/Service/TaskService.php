563704021f250728deacb0a64b26ecbc0002007b
alanchen365 <alanchen365@gmail.com>
2020-12-22 17:14:02 +0800

    合并数组

--- src/Module/Callback/Service/TaskService.php
+++ src/Module/Callback/Service/TaskService.php
@@ -50,7 +50,7 @@ class TaskService extends BaseCallbackService
             $taskCode = md5($taskCode . $push['system_code'] ?? null);
 //            $headers = superEmpty($task->getRequestHeader()) ? $push['request_header'] : json_encode($task->getRequestHeader());
 
-            $headers = array_merge($push['request_header'] ?? [], $task->getRequestHeader());
+            $headers = array_merge($push['request_header'] ?? [], $task->getRequestHeader() ?? []);
 
             $tasks[] = [
                 'task_code' => md5($taskCode),
